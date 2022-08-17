782bb9ce0c243fea9268d5845a253af02b225351
d.pranatha <pranathadoddy@gmail.com>
2022-08-16 20:23:54 -0400

    add validation if file exists

--- get-response.sh
+++ get-response.sh
@@ -4,8 +4,10 @@ file_tugas="tugas1.txt"
 file_tugas_path="$directory_tugas/$file_tugas"
 url=$1
 
-mkdir $directory_tugas
-touch $file_tugas_path 
+if ! [ -f $file_tugas_path ]; then
+	mkdir $directory_tugas
+	touch $file_tugas_path
+fi 
 
 echo -e "\nBody website $url \n $(curl $url)" >> $file_tugas_path
 
