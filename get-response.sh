644f5067127607b8d9f883d73773c6316edbf2ed
d.pranatha <pranathadoddy@gmail.com>
2022-08-16 20:42:27 -0400

    add validation if directory not exists

--- get-response.sh
+++ get-response.sh
@@ -6,13 +6,12 @@ if [ -z "$url" ]; then
 	exit 1
 fi
 
-
 directory_tugas="tugas"
 file_tugas="tugas1.txt"
 file_tugas_path="$directory_tugas/$file_tugas"
 
 if ! [ -f $file_tugas_path ]; then
-	mkdir $directory_tugas
+	! [ -d $directory_tugas ] && mkdir $directory_tugas
 	touch $file_tugas_path
 fi 
 
